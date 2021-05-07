01a5ce2bcf49f5f8356ff3fc745fd815df01b45c
ibrahimerkan <76455945+ibrahimerkan@users.noreply.github.com>
2021-05-08 00:42:42 +0300

    Update Soru2.py

--- Soru2.py
+++ Soru2.py
@@ -1,11 +1,13 @@
 for sayi in range(5):
-    sayi=int(input("sayı:"))
-    x=sayi-1
-    while x>1:
-        if sayi%x==0:
-            print('{} sayısı asal degildir'.format(sayi))
+    sayi = int(input("Sayı: "))
+    
+    x = sayi - 1
+    
+    while x > 1:
+        if sayi % x == 0:
+            print('{} sayısı asal degil'.format(sayi))
             break
         else:
-            x-=1
+            x -= 1
     else:
-        print('{} sayısı asaldır'.format(sayi))
+        print('{} sayısı asal'.format(sayi))
